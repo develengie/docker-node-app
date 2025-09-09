693c8c72a1b8b59a7fe96fd1d2f749b710423444
Alexey Ushakov <develengie@gmail.com>
2025-09-10 01:46:42 +0300

    chore: added environment variable

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express();
 
 const PORT = 3000;
 
+const NUMBER_SERVER_HOST =
+    process.env.NUMBER_SERVER_HOST ?? "http://number:3005";
+
 app.get("/", (req, res) => {
-    fetch("http://number:3005")
+    fetch(NUMBER_SERVER_HOST)
         .then((res) => res.json())
         .then((number) => res.send(`Random number - ${number}.`));
 });
